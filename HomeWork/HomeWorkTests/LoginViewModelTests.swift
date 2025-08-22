9166bd925952afeb744398ee96d0867f3bfbe446
tylerpierog <tylerpierog99@gmail.com>
2025-08-22 15:25:50 -0400

    More tests
    Adding in more tests for the loginviewmodel

--- HomeWork/HomeWorkTests/LoginViewModelTests.swift
+++ HomeWork/HomeWorkTests/LoginViewModelTests.swift
@@ -87,14 +87,16 @@ struct LoginViewModelTests {
         #expect(values.last == false)
     }
     
-    @Test func altButtonActionClearsTextFielsAndErrors() async throws {
+    @Test func altButtonActionClearsTextFielsAndErrorsForLoginState() async throws {
         let vm = LoginViewModel(email: "wwer@gmail.com", password: "password#1")
+        vm.state = .login
         
         vm.emailErrorMessage = "Email error"
         vm.passwordErrorMessage = "Password error"
         
         #expect(vm.emailErrorMessage == "Email error")
         #expect(vm.passwordErrorMessage == "Password error")
+        #expect(vm.state == .login)
         
         vm.altButtonAction()
         
@@ -102,6 +104,27 @@ struct LoginViewModelTests {
         #expect(vm.passwordErrorMessage == nil)
         #expect(vm.emailInputString == "")
         #expect(vm.passwordInputString == "")
+        #expect(vm.state == .signUp)
+    }
+    
+    @Test func altButtonActionClearsTextFielsAndErrorsForSignUpState() async throws {
+        let vm = LoginViewModel(email: "wwer@gmail.com", password: "password#1")
+        vm.state = .signUp
+        
+        vm.emailErrorMessage = "Email error"
+        vm.passwordErrorMessage = "Password error"
+        
+        #expect(vm.emailErrorMessage == "Email error")
+        #expect(vm.passwordErrorMessage == "Password error")
+        #expect(vm.state == .signUp)
+        
+        vm.altButtonAction()
+        
+        #expect(vm.emailErrorMessage == nil)
+        #expect(vm.passwordErrorMessage == nil)
+        #expect(vm.emailInputString == "")
+        #expect(vm.passwordInputString == "")
+        #expect(vm.state == .login)
     }
     
     @Test func isLoginDisabledWhenEmailIsEmpty() async throws {
@@ -122,5 +145,33 @@ struct LoginViewModelTests {
         vm.isLoading = true
         
         #expect(vm.isLoginDisabled)
+    }
+    
+    @Test func loginOrSignUpWithEmptyTextFieldsForLoginState() async throws {
+        let vm = LoginViewModel(email: "", password: "")
+        vm.state = .login
+        
+        await vm.loginOrSignUp()
+        
+        #expect(vm.emailErrorMessage == "Email address cannot be empty.")
+        #expect(vm.passwordErrorMessage == "Password cannot be empty.")
+    }
+    
+    @Test func loginOrSignUpClearsErrorsOnSuccessfulSignUp() async throws {
+        let vm = LoginViewModel(email: "", password: "")
+        vm.state = .signUp
+        
+        await vm.loginOrSignUp()
+        
+        #expect(vm.emailErrorMessage == "Email address cannot be empty.")
+        #expect(vm.passwordErrorMessage == "Password cannot be empty.")
+        
+        vm.emailInputString = "test@test.com"
+        vm.passwordInputString = "validpass@@#5"
+        
+        await vm.loginOrSignUp()
+        
+        #expect(vm.emailErrorMessage == nil)
+        #expect(vm.passwordErrorMessage == nil)
     }
 }
