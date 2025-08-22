4c6ff62172a3c67ae71d5a4e2c42e906bc2777f1
tylerpierog <tylerpierog99@gmail.com>
2025-08-22 10:38:34 -0400

    Add files via upload

---
+++ HomeWork/HomeWork/HomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkApp.swift
+//  HomeWork
+//
+//  Created by tyler pierog on 2025-08-22.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeWork/HomeWorkTests/HomeWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkTests.swift
+//  HomeWorkTests
+//
+//  Created by tyler pierog on 2025-08-22.
+//
+
+import Testing
+@testable import HomeWork
+
+struct HomeWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
